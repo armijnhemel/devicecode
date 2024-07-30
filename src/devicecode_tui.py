ebad2585b62e63a89605f4787bb3bcffdb8b5366
Armijn Hemel <armijn@tjaldur.nl>
2024-07-30 19:37:54 +0200

    remove unused code

--- src/devicecode_tui.py
+++ src/devicecode_tui.py
@@ -20,7 +20,7 @@ from textual.binding import Binding
 from textual.containers import Container, Horizontal, VerticalScroll
 from textual.suggester import Suggester
 from textual.validation import Function, ValidationResult, Validator
-from textual.widgets import Footer, Markdown, Static, Tree, TabbedContent, TabPane, Input, Header, DataTable
+from textual.widgets import Footer, Markdown, Tree, TabbedContent, TabPane, Input, Header, DataTable
 
 #from textual.logging import TextualHandler
 
@@ -391,7 +391,6 @@ class DevicecodeUI(App):
 
         # Create a table with the results. The root element will
         # not have any associated data with it.
-        #self.device_data_area = Static()
         self.device_data_area = Markdown()
         self.regulatory_data_area = Markdown()
         self.model_data_area = Markdown()
